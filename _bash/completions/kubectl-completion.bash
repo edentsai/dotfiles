8b56095adda7da5daec891d52d173266aaaf5bed
Eden Tsai <edentsai231@gmail.com>
2017-12-04 15:31:24 +0800

    Update kubectl and minikube completions

--- _bash/completions/kubectl-completion.bash
+++ _bash/completions/kubectl-completion.bash
@@ -2,5 +2,9 @@
 
 # Only source the completion when kubectl installed
 if command -v kubectl &> /dev/null; then
-    source <(kubectl completion bash)
+    # Reference: https://github.com/kubernetes/minikube/issues/844#issuecomment-262587570
+    tmp_file=$HOME/.bash/completions/kubectl-completion-tmp-$$.bash
+    kubectl completion bash > $tmp_file
+    source $tmp_file
+    rm $tmp_file
 fi
