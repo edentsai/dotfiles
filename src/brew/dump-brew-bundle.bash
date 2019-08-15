78a91e1ab648f9833ea039b6c37e478b00b3182d
Eden Tsai <edentsai231@gmail.com>
2019-08-15 10:45:20 +0800

    feat(brew): add script for dump brew bundle

---
+++ src/brew/dump-brew-bundle.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# vim: set filetype=sh
+
+set -o nounset -o errexit -o pipefail
+
+# Only run on MacOS
+os_type=$(uname -s)
+os_type_macos="Darwin"
+if [ "${os_type}" != "${os_type_macos}" ]; then
+    return
+fi
+unset os_type
+unset os_type_macos
+
+# Add "homebrew/bundle" if not installed
+if ! brew tap | grep -o "homebrew/bundle" > /dev/null; then
+    brew tap "homebrew/bundle"
+fi
+
+brew bundle dump --describe --file=/dev/stdout \
+    | sed -e $'s/^# /\\\n# /g' \
+    | tee Brewfile
